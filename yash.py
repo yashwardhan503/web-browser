941edf6a01e1c01518a89a9c6ae4a714932883bd
yashwardhan503 <68800561+yashwardhan503@users.noreply.github.com>
2020-07-26 14:38:05 +0530

    webbrowseryash.py
    it is a webbrowser command which will open what you want

---
+++ yash.py
@@ -0,0 +1,8 @@
+import webbrowser #no pip needed
+a = input('what can i do for you?: ')
+if a.lower() == 'google':
+   webbrowser.open("google.com")
+if a.lower() == 'youtube':
+   webbrowser.open("youtube.com")
+ 
+#so these are the codes, if you need some more websites just do the same but change the name and website, for now if you print google it will open you google.com and same with youtube
